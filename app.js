38175d5228334084aecf666ff136b891bbdc42bd
Kristijonas <kzalys@gmail.com>
2018-07-08 19:15:36 +0100

    Set up basic app

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(morgan(process.env.ENVIRONMENT));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
+
+// The request was not handled by any of the routers, send a 404 error
+app.use(function(req, res){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
 app.listen(port);
 console.log("App started on port: " + port);
